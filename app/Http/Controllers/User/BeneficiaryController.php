e1cbae401291e3310878a1c182e1334d37971663
Habibur5114 <habiburrahman5114@gmail.com>
2026-08-10 17:02:56 +0600

    update

--- app/Http/Controllers/User/BeneficiaryController.php
+++ app/Http/Controllers/User/BeneficiaryController.php
@@ -76,7 +76,7 @@ class BeneficiaryController extends Controller
             'message' => 'OTP sent successfully.',
         ]);
     }
-//   verifyotpsystem
+//   verifyotpsystemm
     public function verifyOtp(Request $request)
     {
         $request->validate([
