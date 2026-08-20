a82b055c26860783a5e24cc5ab8797d0a7bf4891
Habibur5114 <habiburrahman5114@gmail.com>
2026-08-20 08:43:53 +0600

    update

--- database/migrations/2026_06_02_050124_create_beneficiaries_table.php
+++ database/migrations/2026_06_02_050124_create_beneficiaries_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-     
+    // beginning 
     public function up(): void
     {
         Schema::create('beneficiaries', function (Blueprint $table) {
